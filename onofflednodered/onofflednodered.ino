bcfc9bc742f39fbc074dd582ea70671561ab5c8d
ysnd <yusnadi.aunur@gmail.com>
2025-12-16 21:37:23 +0700

    add other project

---
+++ onofflednodered/onofflednodered.ino
@@ -0,0 +1,19 @@
+const int ledPin = 3;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600); // Inisialisasi komunikasi serial
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    if (command == '1') {
+      // Nyalakan LED
+      digitalWrite(ledPin, HIGH);
+    } else if (command == '0') {
+      // Matikan LED
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
